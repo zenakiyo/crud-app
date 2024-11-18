710f61695c58a0c850f7ae40b760e8078fabf86a
zenakiyo <zenakiyo@gmail.com>
2024-11-18 19:37:47 +0530

    add index.html

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/tacos", (req, res) => {
   res.send("get / tacos");
+});
+
+app.post("/tacos", (req, res) => {
+  const { username, password } = req.body;
+
+  res.send(`Hey ${username} your password is ${password}`);
 });
 
 app.listen(3000, () => {
